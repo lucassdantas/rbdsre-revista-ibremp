51d2a5d1db4d9fd036c1536575c12d09b8828d09
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-05-02 22:27:32 -0300

    insert GA tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,15 @@ import { Inter } from "next/font/google";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { CookieBanner } from "@/components/CookieBanner";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template:'%s | Ibremp - Revista Ibremp',
-    default: 'Ibremp - Revista Ibremp'
+    template: "%s | Ibremp - Revista Ibremp",
+    default: "Ibremp - Revista Ibremp",
   },
   description: "Revista Ibremp",
 };
@@ -22,21 +23,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-        <head>
-          <link rel="shortcut icon" href="/image/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.png"/>
+      <head>
+        <link rel="shortcut icon" href="/image/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.png" />
+
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=R8S0B2BZXN`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'R8S0B2BZXN');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
-        <Header/>
-        <main>
-          {children}
-        </main>
-        <CookieBanner/>
-        <Footer/>
+        <Header />
+        <main>{children}</main>
+        <CookieBanner />
+        <Footer />
       </body>
-
     </html>
   );
 }
